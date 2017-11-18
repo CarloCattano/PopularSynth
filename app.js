e421eefac3ff2841442cd2bbb322b38a9a1e2420
unknown <freevibes@gmail.com>
2017-11-18 19:11:13 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require('express');  
+var app = express();  
+var server = require('http').createServer(app);  
+var io = require('socket.io')(server);
+
+var myClients = [];
+
+
+server.listen(8000);
+app.use(express.static(__dirname + '/node_modules'));  
+app.use(express.static(__dirname + '/public')); 
+app.get('/', function(req, res,next) {  
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket){
+	
+	myClients.push(socket.id);
+	console.log("conected " + myClients);
+	
+	socket.on('disconnect', function() {
+		var gone = myClients.indexOf(socket.id);
+		myClients.splice(gone,1); // this method works perfectly for now
+		console.log("conected " + myClients);
+	});
+		clientsNbr = myClients.length;
+		socket.emit('clientsInfo', clientsNbr);	//CLients information sent to all the peers
+		 socket.emit('clientsIDS', myClients);
+		 
+});
---
+++ public/scripts/ExampleList.js
@@ -0,0 +1,46 @@
+var ExampleList = {
+	"Basic" : {
+		"Oscillators" : "oscillator",
+		"Envelope" : "envelope",
+		"Noise" : "noises",
+		"Player" : "player",
+		"Microphone" : "mic"
+	},
+	"Instruments" : {
+		"Synth" : "simpleSynth",
+		"MonoSynth" : "monoSynth",
+		"FMSynth" : "fmSynth",
+		"PolySynth" : "polySynth",
+		"FatOscillator" : "jump",
+		"MetalSynth" : "bembe",
+		"Granular Synthesis" : "grainPlayer",
+	},
+	"Effects" : {
+		"LFO Effects" : "lfoEffects",
+		"PingPongDelay" : "pingPongDelay",
+		"Buses" : "buses",
+		"Spatialization" : "spatialPanner"
+	},
+	"Sequencing / Timing" : {
+		"Step Sequencer" : "stepSequencer",
+		"Events" : "events",
+		"Play Along" : "shiny",
+		"Quantization" : "quantization",
+		"Playback Rate" : "pianoPhase",
+		"Transport Sync" : "daw"
+	},
+	"Signals" : {
+		"Control Voltage" : "signal",
+		"Ramping Values" : "rampTo",
+	},
+	"Visualization" : {
+		"Animation Sync" : "animationSync",
+		"Envelopes": "funkyShape",
+		"Analysis" : "analysis",
+		"Meter" : "meter"
+	},
+	"Misc" : {
+		"Module Loaders" : "require",
+		"Offline Rendering" : "offline"
+	},
+};
