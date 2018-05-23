c82e12bd909f874e4923fb0baaeff368e5cd090c
Charles Flèche <charles.fleche@free.fr>
2018-05-23 12:34:08 +0200

    Implement nice URLs /drone, /granular and /sampler

--- server.js
+++ server.js
@@ -13,6 +13,15 @@ server.listen(8080);
 app.get('/', function (req, res, next) {
     res.sendFile(__dirname + '/home.html');
 });
+app.get('/drone', function (req, res, next) {
+    res.sendFile(__dirname + '/public/Drone_synth.html');
+});
+app.get('/granular', function (req, res, next) {
+    res.sendFile(__dirname + '/public/Granular_synth.html');
+});
+app.get('/sampler', function (req, res, next) {
+    res.sendFile(__dirname + '/public/Sampler.html');
+});
 app.use(express.static(__dirname + '/node_modules'));
 app.use(express.static(__dirname + '/public'));
 
