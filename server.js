b42c83c3f1c619150ba9950e23b8a47e651f878b
Charles Flèche <charles.fleche@free.fr>
2018-05-23 13:26:25 +0200

    Route / round-robin redirects to synths

--- server.js
+++ server.js
@@ -2,10 +2,28 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 
+function* infiniteIteration (array) {
+  let i = -1
+  while (true) {
+    ++i
+    yield array[i % array.length]
+  }
+}
+
 const port = process.env.NODE_PORT || 80
 
+const synthRoutes = [
+  '/drone/sin',
+  '/drone/saw',
+  '/drone/square',
+  '/granular',
+  '/sampler'
+]
+
+const synthRoute = infiniteIteration(synthRoutes)
+
 app.get('/', function (req, res, next) {
-    res.sendFile(__dirname + '/home.html');
+  res.redirect(synthRoute.next().value)
 });
 app.get('/drone', function (req, res, next) {
     res.sendFile(__dirname + '/public/Drone_synth.html');
